ea0ab212979a6f324d4118417b80fc28535f5c3b
MulongXie <dsh15325@163.com>
2022-06-07 11:38:23 +1000

    add data visualization and discard invisible elements

--- data-processing/GUIData.py
+++ data-processing/GUIData.py
@@ -1,5 +1,6 @@
 import cv2
 import json
+import pandas as pd
 
 
 class GUIData:
@@ -12,7 +13,7 @@ class GUIData:
         self.img = cv2.resize(cv2.imread(gui_img_file), (1440, 2560))  # cv2 image, the screenshot of the GUI
         self.vh = json.load(open(gui_vh_file, 'r'))  # json data, the view hierarchy of the GUI
 
-        self.elements = []      # list of element in dictionary {'id':, 'class':...}
+        self.elements = []       # list of element in dictionary {'id':, 'class':...}
         self.element_id = 0
 
     def extract_elements_from_vh(self):
@@ -26,6 +27,9 @@ class GUIData:
         '''
         Recursively extract children from an element
         '''
+        # discard illegal elements
+        if element['bounds'] == [0,0,0,0]:
+            return
         element['id'] = self.element_id
         self.elements.append(element)
         if 'children' in element:
@@ -37,3 +41,16 @@ class GUIData:
             # replace wordy 'children' with 'children_id'
             del element['children']
         del element['ancestors']
+
+    def visualize_elements(self):
+        board = self.img.copy()
+        for ele in self.elements:
+            print(ele, '\n')
+            bounds = ele['bounds']
+            clip = self.img[bounds[1]: bounds[3], bounds[0]: bounds[2]]
+            cv2.rectangle(board, (bounds[0], bounds[1]), (bounds[2], bounds[3]), (0, 255, 0), 3)
+            cv2.imshow('clip', cv2.resize(clip, (clip.shape[1] // 3, clip.shape[0] // 3)))
+            cv2.imshow('ele', cv2.resize(board, (board.shape[1] // 3, board.shape[0] // 3)))
+            if cv2.waitKey() == ord('q'):
+                break
+        cv2.destroyAllWindows()
