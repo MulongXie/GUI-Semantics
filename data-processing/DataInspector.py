b645cb0c4bdf993bbce3e939c6dcf0fda9358680
MulongXie <dsh15325@163.com>
2022-06-24 08:30:02 +1000

    store image and json dictory separately

--- data-processing/DataInspector.py
+++ data-processing/DataInspector.py
@@ -6,9 +6,10 @@ from GUIData import GUIData
 
 
 class DataInspector:
-    def __init__(self, data_directory='E:\\Mulong\\Datasets\\gui\\rico\\combined\\all'):
+    def __init__(self, img_directory, json_directory):
         # the directory to store json file and image file
-        self.data_directory = data_directory
+        self.img_directory = img_directory
+        self.json_directory = json_directory
 
         # files' paths, list of string
         self.img_files = None
@@ -17,24 +18,39 @@ class DataInspector:
         self.guis = []  # list of GUIData objects
         self.gui_id = 0
 
-    def get_all_img_files_on_data_directory(self, img_type='.jpg'):
-        self.img_files = glob(pjoin(self.data_directory, '*' + img_type))
+    def get_all_img_files(self, img_type='.jpg'):
+        self.img_files = glob(pjoin(self.img_directory, '*' + img_type))
 
-    def get_all_json_files_on_data_directory(self):
-        self.json_files = glob(pjoin(self.data_directory, '*.json'))
+    def get_all_json_files(self):
+        self.json_files = glob(pjoin(self.json_directory, '*.json'))
 
-    def generate_file_path(self, file_name, file_type='.jpg'):
-        name = file_name.replace('/', '\\').split('\\')[-1].split('.')[0]
-        target_file = pjoin(self.data_directory, name + file_type)
+    def generate_file_path(self, source_file, target_file_type='.jpg'):
+        '''
+        Given a source JSON file, generate the corresponding image file path or Vice Versa
+        :param source_file:  the given source file, to get the file name
+        :param target_file_type: the target file type to generate
+        :return: the generated file path
+        '''
+        name = source_file.replace('/', '\\').split('\\')[-1].split('.')[0]
+        target_file = None
+        # generate image path
+        if target_file_type in ('.jpg', '.png'):
+            target_file = pjoin(self.img_directory, name + target_file_type)
+        # generate json path
+        elif target_file_type == '.json':
+            target_file = pjoin(self.json_directory, name + target_file_type)
         return target_file
 
-    def load_gui(self, image_file, json_file):
+    def init_gui(self, image_file, json_file):
+        '''
+        Initiate a GUI object for data management
+        '''
         gui = GUIData(self.gui_id, image_file, json_file)
         self.gui_id += 1
         self.guis.append(gui)
         return gui
 
-    def inspect_gui_img_and_element(self, img_file, json_file, extract_from='vh', save_as_df=False):
+    def load_gui_img_and_element(self, img_file, json_file, extract_from='vh', save_as_df=False):
         '''
         Inspect GUI by visualizing the image and printing out the json file of attributes
         :param img_file: GUI image file path
@@ -44,7 +60,7 @@ class DataInspector:
             @'semantic' - from the semantic json file processed by Rico
         :param save_as_df: Boolean, if True, save the elements into csv where the row title is attribute
         '''
-        gui = self.load_gui(img_file, json_file)
+        gui = self.init_gui(img_file, json_file)
         if extract_from == 'vh':
             gui.extract_elements_from_vh()
         else:
