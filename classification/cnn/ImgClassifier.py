3d3ebf5a7cc2965826f568531fbc9f27bda308c6
MulongXie <dsh15325@163.com>
2022-07-05 11:10:30 +1000

    show the image with its predicted label

--- classification/cnn/ImgClassifier.py
+++ classification/cnn/ImgClassifier.py
@@ -20,15 +20,26 @@ class ImgClassifier:
         self.image_net_cls = [line.split(':')[-1][:-1].replace('\'', '').replace(',', '') for line in open(image_net_file, 'r')]
         self.resnet = ResNet50(backend=keras.backend, layers=keras.layers, models=keras.models, utils=keras.utils)
 
-    def predict_images(self, img_files):
+    def predict_images(self, img_files, show=False):
         images = []
+        orgs = []
         for img_file in img_files:
             img = cv2.imread(img_file)
+            orgs.append(img)
             images.append(img_to_array(cv2.resize(img, (224,224))))
         x = resnet50.preprocess_input(np.array(images), 'channels_last')
         predictions = self.resnet.predict(x)
         labels = []
         for pred in predictions:
             labels.append(self.image_net_cls[np.argmax(pred)])
+
+        if show:
+            for i in range(len(orgs)):
+                print(labels[i])
+                cv2.imshow('img', orgs[i])
+                key = cv2.waitKey()
+                if key == ord('q'):
+                    break
+            cv2.destroyWindow('img')
         return labels
 
